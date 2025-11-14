e5820a17c2b057f03ea6763ff027cbacf460d7cd
Antoine Fricker <antoine@marmelab.com>
2025-11-14 12:43:06 +0100

    complete styling

--- src/ThemeWrapper.ts
+++ src/ThemeWrapper.ts
@@ -1,3 +1,5 @@
+const COMPONENTS_ATTRIBUTES = ['primary'] as const;
+
 const template = document.createElement('template');
 template.innerHTML = `
         <style>
@@ -9,12 +11,32 @@ template.innerHTML = `
         `;
 
 export class ThemeWrapper extends HTMLElement {
+  private primary: string = '#000000';
   private shadow: ShadowRoot;
   constructor() {
     super();
 
     this.shadow = this.attachShadow({ mode: 'open' });
     this.shadow.appendChild(template.content.cloneNode(true));
+  }
+
+  static get observedAttributes() {
+    return COMPONENTS_ATTRIBUTES;
+  }
+
+  attributeChangedCallback(name: (typeof COMPONENTS_ATTRIBUTES)[number], oldValue: string | null, newValue: string | null) {
+    console.log(`${name} changed from ${oldValue} to ${newValue}`);
+    if (name === 'primary') {
+      this.primary = newValue || '#000000';
+      const style = document.createElement('style');
+      style.textContent = `
+        :host {
+            --primary-color: ${this.getAttribute('primary') || '#000'};
+        }
+    `;
+      this.shadow.appendChild(style);
+    }
+    console.warn(`Invalid attribute name: ${name}`);
   }
 }
 
